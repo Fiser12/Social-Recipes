7f1c11689af589b1ff9bb756e15ece43c5c26928
Fiser <fiser12@icloud.com>
2018-02-07 00:12:28 +0100

    App converted in Session Microservice

---
+++ Session/src/Application/DataTransformer/Session/UserDTODataTransform.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Social Recipes project.
+ *
+ * Copyright (c) 2017 LIN3S <ruben@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Session\Application\DataTransformer\Session;
+
+use BenGorUser\User\Application\DataTransformer\UserDTODataTransformer;
+
+/**
+ * @author Ruben Garcia Hernando <ruben@lin3s.com>
+ */
+class UserDTODataTransform extends UserDTODataTransformer
+{
+    public function read() : array
+    {
+        $readParent = parent::read();
+
+        $firstName = null;
+        $lastName = null;
+        $name = null;
+
+        return array_merge($readParent, [
+            'facebook_id' => $this->user->facebookId(),
+            'name'        => $name,
+            'first_name'  => $firstName,
+            'last_name'   => $lastName,
+        ]);
+    }
+}
---
+++ Session/src/Infrastructure/Persistence/Doctrine/Migrations/Version20180115202339.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace Session\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20180115202339 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE user ADD facebook_id_id VARCHAR(255) NOT NULL, ADD facebook_access_token_token VARCHAR(255) NOT NULL, ADD facebook_access_token_createdOn DATE NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE user DROP facebook_id_id, DROP facebook_access_token_token, DROP facebook_access_token_createdOn');
+    }
+}
