febd6910a92ba5fe9e3e0f9913783d73800bf89d
Fiser <fiser12@icloud.com>
2018-02-08 19:14:38 +0100

    Now found the send of the cookie

---
+++ CompositeUi/src/Infrastructure/Symfony/HttpAction/TimelineAction.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Php DDD Standard project.
+ *
+ * Copyright (c) 2017-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace CompositeUi\Infrastructure\Symfony\HttpAction;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TimelineAction
+{
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __invoke(Request $request) : Response
+    {
+        return new Response(
+            $this->twig->render('pages/timeline.html.twig')
+        );
+    }
+}
