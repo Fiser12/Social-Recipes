aa68613157f4d3274e8fc3a508d31ebe6f5fccb7
Fiser <fiser12@icloud.com>
2018-02-13 00:27:37 +0100

    Finished the microservice securization

--- Session/src/Infrastructure/Symfony/HttpAction/Session/JWTDecodeAction.php
+++ Session/src/Infrastructure/Symfony/HttpAction/Session/JWTDecodeAction.php
@@ -14,22 +14,29 @@ namespace Session\Infrastructure\Symfony\HttpAction\Session;
 use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
 use Session\Application\Query\Session\JWTDecodeHandler;
 use Session\Application\Query\Session\JWTDecodeQuery;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class JWTDecodeAction
 {
     private $handler;
+    private $container;
 
-    public function __construct(JWTDecodeHandler $handler)
+    public function __construct(JWTDecodeHandler $handler, ContainerInterface $container)
     {
         $this->handler = $handler;
+        $this->container = $container;
     }
 
     public function __invoke(Request $request): JsonResponse
     {
+        if ($this->container->getParameter('secret') !== $request->headers->get('token-api')) {
+            return new JsonResponse('Secret doesnt exist', 401);
+        }
+
         if (!$jwt = $request->get('jwt')) {
-            return new JsonResponse('JWT query not exist', 400);
+            return new JsonResponse('JWT query not exist', 401);
         }
 
         try {
