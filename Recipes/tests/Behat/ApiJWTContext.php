846cfc319eadcb97309ddb2b501bf65857b9d032
Fiser12 <ruben.garcia@opendeusto.es>
2018-02-12 20:33:17 +0000

    Merged in refactor/recipes-microservice-created (pull request #12)
    Microservice inicialice
    
    * WIP
    
    * Finished the microservice inicialization
    
    Approved-by: Fiser12 <ruben.garcia@opendeusto.es>

---
+++ Recipes/tests/Behat/ApiJWTContext.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Behat;
+
+use Behat\Behat\Context\Context;
+use GuzzleHttp\Client;
+
+class ApiJWTContext implements Context
+{
+    private $client;
+
+    public function __construct(Client $client)
+    {
+
+        $this->client = $client;
+    }
+
+    /**
+     * @When /^I request (GET|PUT|POST|DELETE|PATCH) with JWT to "([^"]*)" in the cookie$/
+     */
+    public function sendRequestWithJWT($method, $url)
+    {
+
+    }
+}
